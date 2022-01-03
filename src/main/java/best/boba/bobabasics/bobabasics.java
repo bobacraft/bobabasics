9348faaf67c7c17b82c9f49f7b8dc16e9517b4fa
BBaoVanC <bbaovanc@bbaovanc.com>
2022-01-03 14:59:48 -0600

    Revert "Add MockBukkit (no tests yet)"
    This reverts commit 3bc8c3126d815c130675bf04d7cfe01f4e3d35ce.

--- src/main/java/best/boba/bobabasics/bobabasics.java
+++ src/main/java/best/boba/bobabasics/bobabasics.java
@@ -1,20 +1,8 @@
 package best.boba.bobabasics;
 
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.JavaPluginLoader;
-
-import java.io.File;
 
 public class bobabasics extends JavaPlugin {
-    public bobabasics() {
-        super();
-    }
-
-    protected bobabasics(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
-        super(loader, description, dataFolder, file);
-    }
-
     @Override
     public void onEnable() {
         this.getCommand("echo").setExecutor(new CommandEcho());
