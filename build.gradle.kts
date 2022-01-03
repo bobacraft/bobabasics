3bc8c3126d815c130675bf04d7cfe01f4e3d35ce
BBaoVanC <bbaovanc@bbaovanc.com>
2022-01-03 00:04:08 -0600

    Add MockBukkit (no tests yet)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 }
 
 dependencies {
+    implementation("junit:junit:4.13.2")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.17:1.13.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
